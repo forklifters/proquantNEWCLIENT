33df61a843c84333e8ba3aa11c6272d3731c5c05
LordWestcott <ollyfilmer@yahoo.co.uk>
2018-09-11 16:36:28 +0100

    working with icon

--- ProQuant/ProQuant.Android/MainActivity.cs
+++ ProQuant/ProQuant.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace ProQuant.Droid
 {
-    [Activity(Label = "ProQuant", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "ProQuant", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
